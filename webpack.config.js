b1396014e9cd1404c9eed91d5aa533d05a0f01cc
王冰 <421768544@qq.com>
2019-07-18 21:43:38 +0800

    feat: 完善模板
    Change-Id: Ia0bf87b8847fedcaf626b6c8a1c4f12d2379c33e

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const config = {
+  entry: './src/index.js', // 不写默认也是它 
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(mp4|jpg|png)$/,
+        use: ['file-loader']
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      title: 'hehe ~~~',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
+  devServer: {
+    contentBase: "./dist",
+    progress: true,
+    hot: true,
+    port: 3000,
+  }
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.plugins.push(new CleanWebpackPlugin());
+  }
+  return config;
+};
