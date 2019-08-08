c7748029e3fd034b984e6957e17984004cf75b29
王冰 <421768544@qq.com>
2019-08-08 19:49:02 +0800

    feat: husky
    Change-Id: Ib2b704e6df6ae71a731ec547308de8d7358a40fc

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,28 @@
-const path = require('path');
+const path = require('path')
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-const babelConfig = require('./babel.base');
-
+const babelConfig = require('./babel.base')
 
 const config = {
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: 'js/[name].[hash].js',
+    filename: 'js/[name].[hash].js'
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            ...babelConfig,
-            babelrc: false,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              ...babelConfig,
+              babelrc: false
+            }
           }
-        }, {
-          loader: 'eslint-loader',
-          options: {
-            fix: true,
-            enforce: 'pre',
-        }
-        }]
+        ]
       },
       {
         test: /\.css$/,
@@ -36,32 +31,32 @@ const config = {
       {
         test: /\.(mp4|jpg|png)$/,
         use: ['file-loader']
-      },
-    ],
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../public/index.html'),
-      title: 'wb-cli-react',
-    }),
+      title: 'wb-cli-react'
+    })
   ],
   resolve: {
     extensions: ['.js', '.jsx'],
     alias: {
-      '@': path.resolve(__dirname, '../src'),
+      '@': path.resolve(__dirname, '../src')
     }
   },
   devServer: {
     contentBase: path.resolve(__dirname, '../dist'),
     progress: true,
     hot: true,
-    port: 3000,
-  },
-};
+    port: 3000
+  }
+}
 
 module.exports = (env, argv) => {
   if (argv.mode === 'development') {
-    config.plugins.push(new CleanWebpackPlugin());
+    config.plugins.push(new CleanWebpackPlugin())
   }
-  return config;
-};
+  return config
+}
